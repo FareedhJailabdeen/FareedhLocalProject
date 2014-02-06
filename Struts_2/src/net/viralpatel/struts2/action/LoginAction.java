2e4f7c66d6f550b7df9f419c5761d2ee641ca5cf
fareedh@vgw.co <fareedh@vgw.co>
2014-02-06 20:09:19 +0530

    Revert "mast"
    This reverts commit e95e70ce819f6974ac8aa47e1251e0b7823c9bde.

--- Struts_2/src/net/viralpatel/struts2/action/LoginAction.java
+++ Struts_2/src/net/viralpatel/struts2/action/LoginAction.java
@@ -1,7 +1,7 @@
 package net.viralpatel.struts2.action;
 
 import com.opensymphony.xwork2.ActionSupport;
-//this changes from master...mast
+//this changes from master
 public class LoginAction extends ActionSupport {
     private String username;
     private String password;
