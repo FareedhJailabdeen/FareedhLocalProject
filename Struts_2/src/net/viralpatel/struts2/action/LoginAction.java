b91540c4ef303e434f80e2208507c74b65409917
fareedh@vgw.co <fareedh@vgw.co>
2014-02-06 12:53:37 +0530

    edit master

--- Struts_2/src/net/viralpatel/struts2/action/LoginAction.java
+++ Struts_2/src/net/viralpatel/struts2/action/LoginAction.java
@@ -1,7 +1,7 @@
 package net.viralpatel.struts2.action;
 
 import com.opensymphony.xwork2.ActionSupport;
-//https://github.com/FareedhJailabdeen/FareedhLocalProject.git
+//this changes from master
 public class LoginAction extends ActionSupport {
     private String username;
     private String password;
