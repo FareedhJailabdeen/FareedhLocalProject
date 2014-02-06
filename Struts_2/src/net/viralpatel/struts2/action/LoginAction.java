d8365fc0839fa58011be98b4cb10746d7f89cf7d
fareedh@vgw.co <fareedh@vgw.co>
2014-02-06 17:26:44 +0530

    this is fareedh

--- Struts_2/src/net/viralpatel/struts2/action/LoginAction.java
+++ Struts_2/src/net/viralpatel/struts2/action/LoginAction.java
@@ -1,7 +1,7 @@
 package net.viralpatel.struts2.action;
 
 import com.opensymphony.xwork2.ActionSupport;
-//this is for hot fix...123
+//this is for hot fix...123 hai this is fareedh
 public class LoginAction extends ActionSupport {
     private String username;
     private String password;
